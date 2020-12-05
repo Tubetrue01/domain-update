6cf04ea5624b7e2abb415ae32eda7111cb581cd1
tubetrue01 <Tubetrue01@gmail.com>
2020-12-06 00:57:36 +0800

    更新 Config 初始化信息

--- config/config.go
+++ config/config.go
@@ -1,9 +1,38 @@
 package config
 
+import (
+	"flag"
+	"os"
+)
+
 type Config struct {
 	AccessKeyID     string
 	AccessKeySecret string
 	Domain          string
 	Email           string
 	EmailAuthCode   string
+}
+
+var command = &Config{}
+
+func init() {
+	flag.StringVar(&command.AccessKeyID, "k", "", "AccessKeyId")
+	flag.StringVar(&command.AccessKeySecret, "s", "", "AccessKeySecret")
+	flag.StringVar(&command.Domain, "d", "", "Domain")
+	flag.StringVar(&command.Email, "e", "", "QQ Email")
+	flag.StringVar(&command.EmailAuthCode, "a", "", "authCode of Email")
+	flag.Parse()
+
+	if !(command.AccessKeyID != "" && command.AccessKeySecret != "" && command.Domain != "" ||
+		command.Email != "" && command.EmailAuthCode != "") {
+
+		flag.Usage()
+		os.Exit(1)
+
+	}
+}
+
+// ObtainCommand 返回初始化之后的 Config 对象
+func ObtainCommand() *Config {
+	return command
 }

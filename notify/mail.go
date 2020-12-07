e8d0283d05774686b4ed66e62510a143dcde94b2
tubetrue01 <Tubetrue01@gmail.com>
2020-12-07 15:49:36 +0800

    添加多邮箱发送的支持

--- notify/mail.go
+++ notify/mail.go
@@ -18,17 +18,11 @@ type Mail struct{}
 
 // DoNotify 发送 Email 信息
 func (m *Mail) DoNotify(config *config.Config, content interface{}) {
-	e := email.NewEmail()
-	e.From = config.Email
-	e.To = []string{config.Email}
-	e.Subject = "IP 变更通知"
-	e.Text = []byte(fmt.Sprintf("IP 地址已经发生变化，新的 IP 地址为：%s\n", content))
+	emails := strings.Split(config.Email, ":")
+	codes := strings.Split(config.EmailAuthCode, ":")
 
-	addr := strings.Join([]string{smtpHost, smtpPort}, ":")
-
-	err := e.Send(addr, smtp.PlainAuth("", config.Email, config.EmailAuthCode, smtpHost))
-	if err != nil {
-		log.Fatal(err)
+	for index, em := range emails {
+		sendEmail(em, codes[index], content)
 	}
 
 }
@@ -36,4 +30,20 @@ func (m *Mail) DoNotify(config *config.Config, content interface{}) {
 // DoNotifyBefore 执行通知前的预处理操作
 func (m *Mail) DoNotifyBefore(config *config.Config, content interface{}) {
 	m.DoNotify(config, content)
+}
+
+// sendEmail 发送邮件
+func sendEmail(emails, code string, content interface{}) {
+	e := email.NewEmail()
+	e.From = emails
+	e.To = []string{emails}
+	e.Subject = "IP 变更通知"
+	e.Text = []byte(fmt.Sprintf("IP 地址已经发生变化，新的 IP 地址为：%s\n", content))
+
+	addr := strings.Join([]string{smtpHost, smtpPort}, ":")
+
+	err := e.Send(addr, smtp.PlainAuth("", emails, code, smtpHost))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
